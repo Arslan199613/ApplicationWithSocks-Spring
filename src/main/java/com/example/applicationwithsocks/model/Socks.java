479796e8fe07cb21f07aa7bf6eb43ed607fc81da
Samsung <arslan.zakirov.1996@mail.ru>
2023-02-05 13:58:55 +0300

    CourseWork with Socks,изменил валидацию полей

--- src/main/java/com/example/applicationwithsocks/model/Socks.java
+++ src/main/java/com/example/applicationwithsocks/model/Socks.java
@@ -4,31 +4,31 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
 import javax.validation.constraints.*;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 public class Socks {
-    @NotEmpty(message = "Данное поле не может быть пустым")
+
+    @NotBlank
     private Color color;
-    @NotEmpty(message = "Данное поле не может быть пустым")
+    @NotBlank
     private Size size;
     @Positive
-    private  int quantity;
+    @NotEmpty
+    private int quantity;
     @Positive
     @NotEmpty
-    @Min(value = 0,message = "Процентное содержание хлопка не может быть меньше 0")
-    @Max(value = 100,message = "Процентное содержание хлопка не может быть больше 100%")
     private int cottonPartAsAPercentage;
 
-    @NotEmpty
+    @NotBlank
     private Type type;
 
     @Override
     public String toString() {
-        return type+ " " +color + " " + size + " " + quantity + " " + cottonPartAsAPercentage;
+        return type + " " + color + " " + size + " " + quantity + " " + cottonPartAsAPercentage;
     }
 }
